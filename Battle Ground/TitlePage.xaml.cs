66591f7a635140e3c4b92bb244912c1ef7967c88
Harrison <Harrison@HARRISON>
2017-03-14 15:06:07 -0400

    MainPage.xaml Renamed to TitlePage as first point of entry for the program

--- Battle Ground/TitlePage.xaml.cs
+++ Battle Ground/TitlePage.xaml.cs
@@ -20,9 +20,9 @@ namespace Battle_Ground
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class TitlePage : Page
     {
-        public MainPage()
+        public TitlePage()
         {
             this.InitializeComponent();
         }
