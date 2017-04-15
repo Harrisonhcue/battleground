f5508e806595d15bf8e3b775e012b6ad9775fb70
Sohail <Sohail@DESKTOP-RDCSP0M>
2017-04-14 21:01:28 -0400

    Character classes for each new character

---
+++ Battle Ground/Characters/RedSeal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle_Ground.Characters
+{
+    class RedSeal : Character
+    {
+        public RedSeal()
+        {
+            // Sets unique values for the character name, image source, attack, and health.
+            _charName = "RedSeal";
+            _charImageSource = "ms-appx:///Assets/RedSeal.png";
+            _attk = 10;
+            _health = 100;
+        }
+
+        // Sets the 4 attacks that the player can choose if they have chosen the character "Adventurer"
+        public override void Attack1(Player _playerAttackingWithRedSeal, Player _playerAttackedByRedSeal)
+        {
+            Stab(_playerAttackingWithRedSeal, _playerAttackedByRedSeal);
+        }
+    }
+}
---
+++ Battle Ground/Characters/SkeletonKnight.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle_Ground.Characters
+{
+    class SkeletonKnight : Character
+    {
+        public SkeletonKnight()
+        {
+            // Sets unique values for the character name, image source, attack, and health.
+            _charName = "Skeleton Knight";
+            _charImageSource = "ms-appx:///Assets/SkeletonKnight.png";
+            _attk = 10;
+            _health = 100;
+        }
+
+        // Sets the 4 attacks that the player can choose if they have chosen the character "Adventurer"
+        public override void Attack1(Player _playerAttackingWithSkeletonKnight, Player _playerAttackedBySkeletonKnight)
+        {
+            Stab(_playerAttackingWithSkeletonKnight, _playerAttackedBySkeletonKnight);
+        }
+    }
+}
